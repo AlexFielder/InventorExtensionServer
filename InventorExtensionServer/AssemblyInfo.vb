1b4384118b9a4f07470bab354da54f8b92d36acd
alex fielder <alex.fielder@gmail.com>
2019-04-08 14:21:49 +0100

    Initial Commit of basic template

---
+++ InventorExtensionServer/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("InventorExtensionServer")>
+<Assembly: AssemblyDescription("InventorExtensionServer")>
+<Assembly: AssemblyCompany("Autodesk, Inc.")>
+<Assembly: AssemblyProduct("Autodesk Inventor")>
+<Assembly: AssemblyCopyright("Copyright ?2012 Autodesk, Inc.")>
+<Assembly: AssemblyTrademark("")>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("023443ba-571c-4f1a-90c7-d6b00ec5241f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.*")>
